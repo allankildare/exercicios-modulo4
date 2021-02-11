b6bae0182aebf8dc555c087901e750a3db3d3f0c
Allan Kildare <allankildare27@gmail.com>
2021-02-11 18:31:49 -0300

    arquivos iniciais adicionados

---
+++ destructuring.js
@@ -0,0 +1,13 @@
+/* Destructuring - Desestruturação
+Tendo a função abaixo, usando destructuring, reescreva-a de modo que fique em apenas 2 linhas de código
+function handleMouseMove(event) {
+    const clientX = event.clientX
+    const clientY = event.clientY
+    console.log(clientX, clientY)
+}
+*/
+
+function handleMouseMove(event) {
+    const {clientX, clientY} = event
+    console.log(clientX, clientY);
+}
